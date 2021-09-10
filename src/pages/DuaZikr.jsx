5e726becd886f5ecbceefe04affc24e1f862d192
rabius-sunny <rabiussunnyc3x@gmail.com>
2021-09-10 21:37:26 +0600

    dua initial done

--- src/pages/DuaZikr.jsx
+++ src/pages/DuaZikr.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import c from '../styles/dua.module.css'
 
@@ -6,6 +6,7 @@ export default function DuaZikr() {
 
     const [name, setName] = useState([])
     const [dua, setDua] = useState([])
+    const [selected, setSelected] = useState([])
 
     useEffect(() => {
         fetch('http://apnardeenijiggasa.com/backend/api/v1/duaname')
@@ -19,17 +20,16 @@ export default function DuaZikr() {
             .catch(err => console.log(err.message))
     }, [])
 
+
     const handleDuaNumber = id => {
-        let result = dua.filter(i => i[2].toString() === id.toString())
+        let result = dua.filter(i => i[1].toString() === id.toString())
+        setSelected(result)
         console.log(result)
     }
 
 
-    // console.log(dua.length)
-    // dua.slice(0, 20).map(i => console.log(i))
 
     let width = window.innerWidth < 1230
-
     return <div className={`${width ? 'container' : ''} my-4`}>
         <div className="row">
             <div className="col-md-3">
@@ -39,22 +39,38 @@ export default function DuaZikr() {
                         <ul>
                             {
                                 name.map((item, index) => {
-                                    if (item[4] !== '') {
-                                        return <li key={index}><button onClick={() => handleDuaNumber(item[2])}>{item[4]}</button></li>
-                                    } else return
+                                    return <li key={index}><button onClick={() => handleDuaNumber(item[0])}>{item[1]}</button></li>
                                 })
                             }
                         </ul>
                     </div>
                 </div>
             </div>
-            <div className="col-md-6">
+            <div className="col-md-7">
                 <div className="dua">
                     <h1 className="text-center">দোয়া ও যিকরসমূহ</h1>
-
+                    {
+                        selected?.map((item, index) => <div className={c.duaContainer} key={index}>
+                            <p className={c.arabic}>{item[6]}</p> <hr style={{ color: '#eee', height: '1px' }} />
+                            <div className={c.transliteration}>
+                                <p><strong>উচ্চারণঃ</strong></p>
+                                <p>{item[7]}</p>
+                            </div> <hr style={{ color: '#eee', height: '1px' }} />
+                            <div className={c.translation}>
+                                <p><strong>অর্থঃ</strong></p>
+                                <p>{item[8]}</p>
+                            </div>
+                            {
+                                item[10] && <div className={c.ref}>
+                                    <p><strong>রেফারেন্সঃ</strong></p>
+                                    <p>{item[10]}</p>
+                                </div>
+                            }
+                        </div>)
+                    }
                 </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-2">
                 <div className="right">
                     <h3 className="text-center">ফন্ট পরিবর্তন</h3>
                 </div>
